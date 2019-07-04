cdf8a7bfe20c3c345a1247b9774302861cf3887b
Maayan Tzoubari <maayan.tzoubari@gmail.com>
2019-07-04 13:29:39 +0200

    checkboxes with each vin input appearing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import createStreamerFrom from './api/streamer'
-import generateCarData from './api/data-generator'
+//import createStreamerFrom from './api/streamer'
+//import generateCarData from './api/data-generator'
 import createCarStreamer from './api/car-data-streamer'
 import Button from './components/Button'
 import Checkbox from './components/Checkbox'
@@ -10,32 +10,58 @@ import Input from './components/Input'
 import EventNotification from './components/EventNotification'
 
 class App extends Component {
-  streamer = createStreamerFrom(() => generateCarData('12345678901234567'))
+  //streamer = createStreamerFrom(() => generateCarData('12345678901234567'))
   state = {
     carData: {},
   }
+  listEvents = []
+  listVins = []
   setVin = this.setVin.bind(this)
   updateState = carData => {
     this.setState({ carData })
   }
   componentWillMount() {
+    console.log('will mount')
     console.log(this.state.carData)
   }
-  componentDidMount() {
+  /*componentDidMount() {
     this.streamer.subscribe(this.updateState)
     this.streamer.start()
-  }
+  }*/
   setVin() {
-    console.log('working')
-    console.log(this.vinInput.value)
+    //console.log('working')
+    //console.log(this.vinInput.value)
     const carStreamer = createCarStreamer(this.vinInput.value)
-    console.log(this.state.carData)
+    //console.log(this.state.carData)
     carStreamer.start()
     carStreamer.subscribe(carData => {
       this.setState({ carData })
     })
+    this.listEvents.push(this.state.carData)
+    //console.log(this.listEvents)
+    this.listVins.push(this.state.carData.vin)
+    //console.log(this.listVins);
   }
   render() {
+    var allEvents = []
+    for (let i = 0; i < this.listEvents.length; i++) {
+      //if (this.state.carData.vin==this.listEvents[i].vin)
+      allEvents.push(
+        <EventNotification
+          carEvent={this.state.carData}
+          key={this.listEvents[i].vin}>
+          {this.listEvents[i]}
+        </EventNotification>,
+      )
+    }
+    var allVins = []
+    for (let i = 0; i < this.listVins.length; i++) {
+      allVins.push(
+        <Checkbox defaultChecked key={this.listVins[i]}>
+          {this.listVins[i]}
+        </Checkbox>,
+      )
+    }
     return (
       <div className="App">
         <header className="App-header">
@@ -46,8 +72,8 @@ class App extends Component {
           </p>*/}
           <Input newVin={x => (this.vinInput = x)}></Input>
           <Button setVin={this.setVin}>Add+</Button>
-          <Checkbox></Checkbox>
-          <EventNotification carEvent={this.state.carData}></EventNotification>
+          {allVins}
+          {allEvents}
           <div>{JSON.stringify(this.state.carData)}</div>
           <a
             className="App-link"
