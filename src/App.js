7f36cbb6398cb53e91e8ec9a39442492d0954da1
Maayan Tzoubari <maayan.tzoubari@gmail.com>
2019-07-04 16:10:18 +0200

    generating events and adding vins

--- src/App.js
+++ src/App.js
@@ -16,52 +16,40 @@ class App extends Component {
   }
   listEvents = []
   listVins = []
+  unselectedVin = null
   setVin = this.setVin.bind(this)
   updateState = carData => {
     this.setState({ carData })
   }
-  componentWillMount() {
-    console.log('will mount')
-    console.log(this.state.carData)
-  }
-  /*componentDidMount() {
-    this.streamer.subscribe(this.updateState)
-    this.streamer.start()
-  }*/
+
   setVin() {
-    //console.log('working')
-    //console.log(this.vinInput.value)
     const carStreamer = createCarStreamer(this.vinInput.value)
-    //console.log(this.state.carData)
     carStreamer.start()
     carStreamer.subscribe(carData => {
-      this.setState({ carData })
+      this.listEvents.push(carData)
+      this.updateState()
     })
-    this.listEvents.push(this.state.carData)
-    //console.log(this.listEvents)
-    this.listVins.push(this.state.carData.vin)
-    //console.log(this.listVins);
+    this.listVins.push(this.vinInput.value)
+    console.log(this.listVins)
   }
   render() {
-    var allEvents = []
-    for (let i = 0; i < this.listEvents.length; i++) {
-      //if (this.state.carData.vin==this.listEvents[i].vin)
-      allEvents.push(
-        <EventNotification
-          carEvent={this.state.carData}
-          key={this.listEvents[i].vin}>
-          {this.listEvents[i]}
-        </EventNotification>,
-      )
-    }
     var allVins = []
     for (let i = 0; i < this.listVins.length; i++) {
       allVins.push(
-        <Checkbox defaultChecked key={this.listVins[i]}>
+        <Checkbox defaultChecked key={i}>
           {this.listVins[i]}
         </Checkbox>,
       )
     }
+    var allEvents = []
+    for (let i = 0; i < this.listEvents.length; i++) {
+      allEvents.push(
+        <EventNotification carEvent={this.listEvents[i]} key={i}>
+          {this.listEvents[i]}
+        </EventNotification>,
+      )
+    }
+
     return (
       <div className="App">
         <header className="App-header">
@@ -74,7 +62,6 @@ class App extends Component {
           <Button setVin={this.setVin}>Add+</Button>
           {allVins}
           {allEvents}
-          <div>{JSON.stringify(this.state.carData)}</div>
           <a
             className="App-link"
             href="https://reactjs.org"
